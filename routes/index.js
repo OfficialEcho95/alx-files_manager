01de0f544e630fcc9f3782b97c6807e661deced6
Emmanuel Chukwu <officialecho95@outlook.com>
2023-09-04 02:08:23 +0100

    file

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
+
+const FilesController = require('../controllers/FilesController');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController')
-
 
 router.post('/users', UsersController.postNew);
 router.get('/status', AppController.getStatus);
@@ -12,5 +12,6 @@ router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
