5657f71622401c07aa710659bddea0794fbe9c7a
Emmanuel Chukwu <officialecho95@outlook.com>
2023-09-06 19:21:32 +0100

    redis server update

--- utils/redis.js
+++ utils/redis.js
@@ -1,41 +1,43 @@
 const redis = require('redis');
 
-/**
- * RedisClient class for managing a Redis client.
- */
 class RedisClient {
-  /**
-   * Creates a new instance of the RedisClient class.
-   */
   constructor() {
-    // Initialize the Redis client.
-    this.client = redis.createClient();
-    this.client.connected = false;
+    this.client = redis.createClient({ host: 'localhost', port: 6379 });
 
-    // Handle Redis client errors.
+    // Initialize a flag to track the connection status.
+    this.connected = false;
+
+    // Handle Redis client events.
+    this.client.on('connect', () => {
+      this.connected = true;
+      console.log('Connected to Redis');
+    });
+
     this.client.on('error', (err) => {
-      console.error(`${err}`);
+      console.error(`Redis Error: ${err}`);
+    });
+
+    this.client.on('end', () => {
+      this.connected = false;
+      console.log('Disconnected from Redis');
+    });
+  }
+
+  // Updated isAlive() method that returns a Promise.
+  async isAlive() {
+    return new Promise((resolve) => {
+      if (this.connected) {
+        resolve(true);
+      } else {
+        // Listen for the 'connect' event and resolve when connected.
+        this.client.once('connect', () => {
+          resolve(true);
+        });
+      }
     });
   }
 
 
-  /**
-   * Checks if the Redis client is alive (connected).
-   * @returns {boolean} True if the client is connected, otherwise false.
-   */
-  isAlive() {
-    if (this.client.connected) {
-      return true;
-    };
-    return false;
-  }
-
-  /**
-   * Retrieves the value associated with a given key from Redis.
-   * @param {string} key - The key to retrieve the value for.
-   * @returns {Promise<string|null>} A Promise that resolves
-   * to the value or null if the key doesn't exist.
-   */
   async get(key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (err, reply) => {
@@ -48,13 +50,6 @@ class RedisClient {
     });
   }
 
-  /**
-   * Sets a key-value pair in Redis with an expiration time.
-   * @param {string} key - The key to set.
-   * @param {string} value - The value to associate with the key.
-   * @param {number} durationInSeconds - The expiration time in seconds.
-   * @returns {Promise<string>} A Promise that resolves when the key-value pair is set.
-   */
   async set(key, value, durationInSeconds) {
     return new Promise((resolve, reject) => {
       this.client.setex(key, durationInSeconds, value, (err, reply) => {
@@ -67,11 +62,6 @@ class RedisClient {
     });
   }
 
-  /**
-   * Deletes a key and its associated value from Redis.
-   * @param {string} key - The key to delete.
-   * @returns {Promise<number>} A Promise that resolves with the number of keys deleted (0 or 1).
-   */
   async del(key) {
     return new Promise((resolve, reject) => {
       this.client.del(key, (err, reply) => {
@@ -85,7 +75,6 @@ class RedisClient {
   }
 }
 
-// Create an instance of the RedisClient class.
 const redisClient = new RedisClient();
 
 module.exports = redisClient;
